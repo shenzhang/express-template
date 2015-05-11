1c375fb123ff574fd8ec2e444f61ff9b9adae39f
shenzhang <shenzhang722@gamil.com>
2015-05-11 22:53:24 +1000

    change template

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,66 @@
+// gulp
 var gulp = require('gulp');
-var concat = require('gulp-concat');
+
+// plugins
+var connect = require('gulp-connect');
+var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
-var less = require('gulp-less');
-var sourcemaps = require('gulp-sourcemaps');
-var Q = require('q');
-var path = require('path');
-var del = require('del');
-var fs = require('fs-extra');
+var minifyCSS = require('gulp-minify-css');
+var clean = require('gulp-clean');
 
-var PATH = {
-  targetJs : 'public/js',
-  targetCss : 'public/css',
-  bower : 'bower_components'
-}
-
-gulp.task('clean', function() {
-  del.sync(['public/js/main.js']);
+// tasks
+gulp.task('lint', function () {
+	gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(jshint.reporter('fail'));
+});
+gulp.task('clean', function () {
+    gulp.src('./dist/*')
+		.pipe(clean({ force: true }));
+});
+gulp.task('minify-css', function () {
+	var opts = { comments: true, spare: true };
+	gulp.src(['./app/**/*.css', '!./app/bower_components/**'])
+		.pipe(minifyCSS(opts))
+		.pipe(gulp.dest('./dist/'))
+});
+gulp.task('minify-js', function () {
+	gulp.src(['./app/**/*.js', '!./app/bower_components/**'])
+		.pipe(uglify({
+		// inSourceMap:
+		// outSourceMap: "app.js.map"
+    }))
+		.pipe(gulp.dest('./dist/'))
+});
+gulp.task('copy-bower-components', function () {
+	gulp.src('./app/bower_components/**')
+		.pipe(gulp.dest('dist/bower_components'));
+});
+gulp.task('copy-html-files', function () {
+	gulp.src('./app/**/*.html')
+		.pipe(gulp.dest('dist/'));
+});
+gulp.task('connect', function () {
+	connect.server({
+		root: 'app/',
+		port: 8888
+	});
+});
+gulp.task('connectDist', function () {
+	connect.server({
+		root: 'dist/',
+		port: 9999
+	});
 });
 
-gulp.task('javascript', ['clean'], function() {
-  var jsArray = ['jquery/dist/jquery.js', 
-            'angular/angular.js', 
-            'socket.io-client/socket.io.js'];
-  var jsArrayFinal = [];
-  jsArray.forEach(function(js) {
-    jsArrayFinal.push(path.join(PATH.bower, js));
-  });
 
-  return gulp.src(jsArrayFinal)
-    .pipe(sourcemaps.init())
-    .pipe(concat('main.js', {newLine: ';'}))
-    .pipe(uglify())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(PATH.targetJs));
-});
-
-gulp.task('css', ['clean'], function() {
-  
-});
-
-gulp.task('default', ['javascript', 'css']);
+// default task
+gulp.task('default',
+	['lint', 'connect']
+	);
+	
+// build task
+gulp.task('build',
+	['lint', 'minify-css', 'minify-js', 'copy-html-files', 'copy-bower-components', 'connectDist']
+	);
--- public/app/modules/login.js
+++
@@ -1,6 +0,0 @@
-;
-(function() {
-  var module = angular.module('login', []);
-
-  
-})();
